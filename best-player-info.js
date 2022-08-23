47210fd4914277c8fc398720403385cda3b27e1d
Mofassel100 <smmofasselhosain.bba.ac.22@gmail.com>
2022-08-23 16:14:24 +0600

    alert massege add

--- best-player-info.js
+++ best-player-info.js
@@ -1,15 +1,16 @@
 
 
 let allPlayerNames = [];
+function updatePlayerNames(element){
+    if(allPlayerNames.length === 5){
 
-
-
-
-function display(element){
+        alert('please Maximum five playe select');
+        return alert;
+    }
       
     
-    const playerNameAdd= document.getElementById('player-names');
-    playerNameAdd.textContent = '';
+const playerNameAdd= document.getElementById('player-names');
+playerNameAdd.textContent = '';
     
     for (let i = 0; i < element.length; i++){
 
@@ -25,48 +26,37 @@ playerNameAdd.appendChild(li);
 function playerNameAdd(text){
 const playerName =text.parentNode.parentNode.children[0].innerText;
 allPlayerNames.push(playerName); 
-display(allPlayerNames);
+ updatePlayerNames(allPlayerNames);
 const headAdd = document.getElementById('add');
 headAdd.innerText = allPlayerNames.length;
-caculatPlayerExp(allPlayerNames);
-text.disabled=true;
+updatePerPlayerExp(allPlayerNames)
 
+// select btn disable
+text.disabled =true;
 
 
 }
-// btn disabled 
-function btnDisabled ()
-{
-    document.getElementsByClassName('disa').disabled = true;
-    
-}
+
 // ----------------names add end-------------------------
 
 
 // -----calcolat btn one start now-------
 let calcNamesTotal = 1;
-function caculatPlayerExp(element){
-    let calcExpenBtn =1+1;
-    calcExpenBtn.textContent ='';
-    for (let i =0 ; i< element.length; i++){
-    calcExpenBtn = `${i+1}`;
+function updatePerPlayerExp(calculate){
+    let perPlayerRate =1+1;
+    perPlayerRate.textContent ='';
+    for (let i =0 ; i< calculate.length; i++){
+        perPlayerRate = `${i+1}`;
     }
-    return calcExpenBtn;
+    return perPlayerRate;
 }
 
-function totalExpenses (){
+function PlayerExpensesTotal (){
 const previTotalPlayer
-    =caculatPlayerExp (allPlayerNames);
-                       
+    =updatePerPlayerExp (allPlayerNames);                      
 const perPlayerFild = document.getElementById('per-player-cost');
-
 const perPlayerSt= perPlayerFild.value;
 const newPerPlayerFild = parseInt(perPlayerSt);
-if (typeof newPerPlayerFild !=='number'){
-    alert ('Please Enter your number');
-    return alert;
-}
-
 const totalExpent = newPerPlayerFild * previTotalPlayer;
 const playerTotalExpen = document.getElementById('player-total-cost');
 playerTotalExpen.innerText = totalExpent;
@@ -77,7 +67,7 @@ return playerTotalExpen;
 
 document.getElementById('cal-btn').addEventListener('click',function(){
 
-    totalExpenses ()
+    PlayerExpensesTotal ()
     
 })
 
@@ -85,25 +75,17 @@ document.getElementById('cal-btn').addEventListener('click',function(){
 //------------ calcolate  bnt two start ----
 function calcolBtnTwoExpenses ( ){
 
-    const playerTotalExpense = totalExpenses();
+    const playerTotalExpense = PlayerExpensesTotal();
     const playerTotalExpenseS= playerTotalExpense.innerText;
     const playerTotalExpenseNum = parseInt(playerTotalExpenseS)
     const managerInputFildPrice = document.getElementById('manager-cost');
-    
-    
     const managerInputFildStringPrice= managerInputFildPrice.value;
     const newManagerTotalPric = parseInt(managerInputFildStringPrice);
-    
-    
     const coachPriceFild = document.getElementById('coach-cost');
-    
-    
     const coachPriceFildDaSt= coachPriceFild.value;
     const coachTotalPrice = parseInt(coachPriceFildDaSt);
-    
     const totalCost = playerTotalExpenseNum + newManagerTotalPric + coachTotalPrice;
     const totalCalcoCost = document.getElementById('team-all-cost');
-
     totalCalcoCost.innerText =totalCost;
 
 }
